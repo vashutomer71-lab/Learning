fd1f3c56666ee47896c9118894b5fbc8921a06da
vashut <vashu.tomer71@gmail.com>
2025-09-10 20:28:08 +0530

    Mumin dashboard for Widget with DB Validation

--- Test_Cases/test_mumin_dashboard.py
+++ Test_Cases/test_mumin_dashboard.py
@@ -6,23 +6,79 @@ from Utilities.CustomLoggers import LogGen
 
 class TestMuminDashboard:
     logger = LogGen.loggen()
-    @allure.step("Click on the dashboard menu and open the dashboard page")
-    def test_click_mumin_dashboard_menu(self, setup, login):
-        self.page = login
-        self.c_tile = MuminDashboardPage(self.page)
-        self.c_tile.click_dashboard_menu()
-        self.logger.info("******Click on the Dashboard menu****")
+    # @allure.step("Click on the dashboard menu and open the dashboard page")
+    # def test_click_mumin_dashboard_menu(self, setup, login):
+    #     self.page = login
+    #     self.m_tile = MuminDashboardPage(self.page)
+    #     self.m_tile.click_dashboard_menu()
+    #     self.logger.info("******Click on the Dashboard menu****")
 
-    @allure.step("Verify the mumin dashboard heading")
-    def test_dashboard_heading(self, setup, login):
-        self.page = login
-        self.c_tile = MuminDashboardPage(self.page)
-        self.c_tile.verify_mumin_dashboard_heading()
-        self.logger.info("******Verify the mumin dashboard heading****")
+    # @allure.step("Verify the mumin dashboard heading")
+    # def test_dashboard_heading(self, setup, login):
+    #     self.page = login
+    #     self.m_tile = MuminDashboardPage(self.page)
+    #     self.m_tile.verify_mumin_dashboard_heading()
+    #     self.logger.info("******Verify the mumin dashboard heading****")
 
-    @allure.step("Verify the all widgets count with the data base count for the total niyat, active, pending, completed")
-    def test_dashboard_tile(self, setup, login, elaam_prod):
-        self.page = login
-        self.fd_tile = MuminDashboardPage(self.page)
-        self.fd_tile.verify_dashboard_tile_all_filter(elaam_prod)
-        self.logger.info("********Verify the all widgets count with the data base count for the total niyat, active, pending, completed**********")
+    # @allure.step("Verify the all widgets count with the database count for the total niyat, active, pending, completed")
+    # def test_dashboard_tile(self, setup, login, elaam_prod):
+    #     self.page = login
+    #     self.niyats_tile = MuminDashboardPage(self.page)
+    #     self.niyats_tile.verify_dashboard_tile_all_filter(elaam_prod)
+    #     self.logger.info("********Verify the all widgets count with the database count for the total niyat, active, pending, completed**********")
+    #
+    # def test_duration_drop_down_opened(self, setup, login):
+    #     self.page=login
+    #     self.duration_dropdown = MuminDashboardPage(self.page)
+    #     self.duration_dropdown.click_duration_drop_down_and_select_1_month_filter()
+    #     self.logger.info("*******Duration dropdown opened successfully and select 1 month filter successfully**********")
+    #
+    # def test_dashboard_widget_for_1_month(self, setup,login,elaam_prod):
+    #     self.page=login
+    #     self.month_1_filter = MuminDashboardPage(self.page)
+    #     self.month_1_filter.verify_dashboard_tile_1_month_filter(elaam_prod)
+    #     self.logger.info("*****Verify all widget count for 1 month filter successfully.******")
+    #
+    # def test_duration_drop_down_opened_and_select_3_months_option(self, setup, login):
+    #     self.page=login
+    #     self.duration_dropdown = MuminDashboardPage(self.page)
+    #     self.duration_dropdown.click_duration_drop_down_and_select_3_month_filter()
+    #     self.logger.info("*******Duration dropdown opened successfully and select 3 months filter successfully**********")
+    #
+    # def test_dashboard_widget_for_3_months(self, setup, login, elaam_prod):
+    #     self.page = login
+    #     self.month_3_filter = MuminDashboardPage(self.page)
+    #     self.month_3_filter.verify_dashboard_tile_3_month_filter(elaam_prod)
+    #     self.logger.info("*****Verify all widget count for 3 month filter successfully.******")
+    #
+    # def test_duration_drop_down_opened_and_select_6_months_option(self, setup, login):
+    #     self.page = login
+    #     self.duration_dropdown = MuminDashboardPage(self.page)
+    #     self.duration_dropdown.click_duration_drop_down_and_select_6_months_filter()
+    #     self.logger.info("*******Duration dropdown opened successfully and select 6 months filter successfully**********")
+    #
+    # def test_dashboard_widget_for_6_months(self, setup, login, elaam_prod):
+    #     self.page = login
+    #     self.month_6_filter = MuminDashboardPage(self.page)
+    #     self.month_6_filter.verify_dashboard_tile_6_month_filter(elaam_prod)
+    #     self.logger.info("*****Verify all widget count for 6 months filter successfully.******")
+    #
+    # def test_duration_drop_down_opened_and_select_1_year_option(self, setup, login):
+    #     self.page = login
+    #     self.duration_dropdown = MuminDashboardPage(self.page)
+    #     self.duration_dropdown.click_duration_drop_down_and_select_1_year_filter()
+    #     self.logger.info("*******Duration dropdown opened successfully and select 1 year filter successfully**********")
+    #
+    # def test_dashboard_widget_for_1_year(self, setup, login, elaam_prod):
+    #     self.page = login
+    #     self.month_1_year = MuminDashboardPage(self.page)
+    #     self.month_1_year.verify_dashboard_tile_1_year_filter(elaam_prod)
+    #     self.logger.info("*****Verify all widget count for 1 year filter successfully.******")
+
+    def test_total_trophy_count(self,setup,login):
+        self.page=login
+        self.TC= MuminDashboardPage(self.page)
+        self.TC.verify_total_trophy_count()
+
+
+
--- pageObjects/mumin_dashboard_page.py
+++ pageObjects/mumin_dashboard_page.py
@@ -2,6 +2,7 @@ from playwright.sync_api import sync_playwright, Page
 from Test_Cases.conftest import db_connection, elaam_prod
 from Utilities.ReadProperties import ReadConfig
 from tabulate import tabulate
+import re
 
 url = ReadConfig.get_back_end_url()
 
@@ -11,6 +12,13 @@ xpath_Total_Niyat_tile = "(//div[@class='box1 ng-star-inserted'])[1]"
 xpath_Active_Niyat_tile = "(//div[@class='box1 ng-star-inserted'])[2]"
 xpath_Approval_pending_Niyat_tile = "(//div[@class='box1 ng-star-inserted'])[3]"
 xpath_Completed_Niyat_tile = "(//div[@class='box1 ng-star-inserted'])[4]"
+xpath_duration_dropdown = "#mat-select-value-1"
+xpath_1_month = "//span[normalize-space(text())='Last 1 Month']"
+xpath_3_month = "//span[normalize-space(text())='Last 3 Months']"
+xpath_6_month = "//span[normalize-space(text())='Last 6 Months']"
+xpath_1_year = "//span[normalize-space(text())='1 year']"
+xpath_total_trophy_count = "//div[@class='txt_overlay txt_top']"
+xpah_trophies_redeemed= "//p[contains(text(),'Trophies Redeemed')]"
 
 class MuminDashboardPage:
 
@@ -66,6 +74,190 @@ class MuminDashboardPage:
         assert completed_niyat_count_widget == completed_count_db, f"Expected completed niyat count {completed_count_db}, but got on widget {completed_niyat_count_widget}"
         assert total_niyat_count_add_all_status == total_niyat_count_db, f"Expected total niyat count in db {total_niyat_count_db}, but got total niyat count after adding all status{total_niyat_count_add_all_status}"
         print("All assertions passed successfully for mumin dashboard tiles")
+
+    def click_duration_drop_down_and_select_1_month_filter(self):
+        self.page.locator(xpath_duration_dropdown).click()
+        self.page.locator("body").press("ControlOrMeta+Shift+I")
+        # self.page.get_by_role("option", name="Last 1 Month").click()
+        self.page.locator(xpath_1_month).click()
+
+    def verify_dashboard_tile_1_month_filter(self, elaam_prod):
+        self.page.wait_for_timeout(3000)
+        total_niyat_count_widget_for_1_month = int(next(line for line in self.page.inner_text(xpath_Total_Niyat_tile).splitlines() if line.strip()))
+        print("Total Niyat Count for 1 month:", total_niyat_count_widget_for_1_month)
+        active_niyat_count_widget_for_1_month  = int(next(line for line in self.page.inner_text(xpath_Active_Niyat_tile).splitlines() if line.strip()))
+        print("Active Niyat Count for 1 month:", active_niyat_count_widget_for_1_month)
+        approval_pending_niyat_count_widget_for_1_month = int(next(line for line in self.page.inner_text(xpath_Approval_pending_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 1 month:", approval_pending_niyat_count_widget_for_1_month)
+        completed_niyat_count_widget_for_1_month = int(next(line for line in self.page.inner_text(xpath_Completed_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 1 month:", completed_niyat_count_widget_for_1_month)
+
+        # Execute SQL query to fetch data from the database
+        total_niyat_query_for_1_month= "1_month_niyat_tile_count_query"
+        headers, result = elaam_prod(total_niyat_query_for_1_month)
+        db_table_view= tabulate(result, headers=headers, tablefmt="grid")   # Display the database results in a tabular format
+        print("DB table view for 1 month: ", db_table_view)
+        # Process database results to store counts by status
+        total_niyat_count_db_for_1_month = result[0][headers.index('total_count')]
+        active_count_db = result[0][headers.index('active_count')]
+        completed_count_db = result[0][headers.index('completed_count')]
+        approval_pending_db = result[0][headers.index('approval_pending_count')]
+        deactivated_db = result[0][headers.index('deactivated_count')]
+        print("DB Total Niyat:", total_niyat_count_db_for_1_month)
+        print("DB Active:", active_count_db)
+        print("DB Completed:", completed_count_db)
+        print("DB Approval Pending:", approval_pending_db)
+        print("DB Deactivated:", deactivated_db)
+        total_niyat_count_add_all_status = active_count_db + completed_count_db + approval_pending_db + deactivated_db
+
+        assert total_niyat_count_widget_for_1_month == total_niyat_count_db_for_1_month, f"Expected  total niyats count  in db '{total_niyat_count_db_for_1_month}', but got on widget '{total_niyat_count_widget_for_1_month}'"
+        assert active_niyat_count_widget_for_1_month == active_count_db, f"Expected active niyat count in bd {active_count_db}, but got on widget {active_niyat_count_widget_for_1_month}"
+        assert approval_pending_niyat_count_widget_for_1_month == approval_pending_db, f"Expected approval pending niyat count in db {approval_pending_db}, but got on widget ' {approval_pending_niyat_count_widget_for_1_month}"
+        assert completed_niyat_count_widget_for_1_month == completed_count_db, f"Expected completed niyat count {completed_count_db}, but got on widget {completed_niyat_count_widget_for_1_month}"
+        assert total_niyat_count_add_all_status == total_niyat_count_db_for_1_month, f"Expected total niyat count in db {total_niyat_query_for_1_month}, but got total niyat count after adding all status{total_niyat_count_add_all_status}"
+        print("All assertions passed successfully for mumin dashboard tiles for 1 month filter")
+
+    def click_duration_drop_down_and_select_3_month_filter(self):
+        self.page.locator(xpath_duration_dropdown).click()
+        self.page.locator("body").press("ControlOrMeta+Shift+I")
+        # self.page.get_by_role("option", name="Last 1 Month").click()
+        self.page.locator(xpath_3_month).click()
+
+    def verify_dashboard_tile_3_month_filter(self, elaam_prod):
+        self.page.wait_for_timeout(3000)
+        total_niyat_count_widget_for_3_month = int(next(line for line in self.page.inner_text(xpath_Total_Niyat_tile).splitlines() if line.strip()))
+        print("Total Niyat Count for 3 month:", total_niyat_count_widget_for_3_month)
+        active_niyat_count_widget_for_3_month  = int(next(line for line in self.page.inner_text(xpath_Active_Niyat_tile).splitlines() if line.strip()))
+        print("Active Niyat Count for 3 month:", active_niyat_count_widget_for_3_month)
+        approval_pending_niyat_count_widget_for_3_month = int(next(line for line in self.page.inner_text(xpath_Approval_pending_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 3 month:", approval_pending_niyat_count_widget_for_3_month)
+        completed_niyat_count_widget_for_3_month = int(next(line for line in self.page.inner_text(xpath_Completed_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 3 month:", completed_niyat_count_widget_for_3_month)
+
+        # Execute SQL query to fetch data from the database
+        total_niyat_query_for_3_month= "3_month_niyat_tile_count_query"
+        headers, result = elaam_prod(total_niyat_query_for_3_month)
+        db_table_view= tabulate(result, headers=headers, tablefmt="grid")   # Display the database results in a tabular format
+        print("DB table view for 3 month: ", db_table_view)
+        # Process database results to store counts by status
+        total_niyat_count_db_for_3_month = result[0][headers.index('total_count')]
+        active_count_db = result[0][headers.index('active_count')]
+        completed_count_db = result[0][headers.index('completed_count')]
+        approval_pending_db = result[0][headers.index('approval_pending_count')]
+        deactivated_db = result[0][headers.index('deactivated_count')]
+        print("DB Total Niyat:", total_niyat_count_db_for_3_month)
+        print("DB Active:", active_count_db)
+        print("DB Completed:", completed_count_db)
+        print("DB Approval Pending:", approval_pending_db)
+        print("DB Deactivated:", deactivated_db)
+        total_niyat_count_add_all_status = active_count_db + completed_count_db + approval_pending_db + deactivated_db
+
+        assert total_niyat_count_widget_for_3_month == total_niyat_count_db_for_3_month, f"Expected  total niyats count  in db '{total_niyat_count_db_for_3_month}', but got on widget '{total_niyat_count_widget_for_3_month}'"
+        assert active_niyat_count_widget_for_3_month == active_count_db, f"Expected active niyat count in bd {active_count_db}, but got on widget {active_niyat_count_widget_for_3_month}"
+        assert approval_pending_niyat_count_widget_for_3_month == approval_pending_db, f"Expected approval pending niyat count in db {approval_pending_db}, but got on widget ' {approval_pending_niyat_count_widget_for_3_month}"
+        assert completed_niyat_count_widget_for_3_month == completed_count_db, f"Expected completed niyat count {completed_count_db}, but got on widget {completed_niyat_count_widget_for_3_month}"
+        assert total_niyat_count_add_all_status == total_niyat_count_db_for_3_month, f"Expected total niyat count in db {total_niyat_query_for_3_month}, but got total niyat count after adding all status{total_niyat_count_add_all_status}"
+        print("All assertions passed successfully for mumin dashboard tiles for 3 month filter")
+
+    def click_duration_drop_down_and_select_6_months_filter(self):
+        self.page.locator(xpath_duration_dropdown).click()
+        self.page.locator("body").press("ControlOrMeta+Shift+I")
+        # self.page.get_by_role("option", name="Last 1 Month").click()
+        self.page.locator(xpath_6_month).click()
+
+    def verify_dashboard_tile_6_month_filter(self, elaam_prod):
+        self.page.wait_for_timeout(3000)
+        total_niyat_count_widget_for_6_month = int(next(line for line in self.page.inner_text(xpath_Total_Niyat_tile).splitlines() if line.strip()))
+        print("Total Niyat Count for 6 month:", total_niyat_count_widget_for_6_month)
+        active_niyat_count_widget_for_6_month  = int(next(line for line in self.page.inner_text(xpath_Active_Niyat_tile).splitlines() if line.strip()))
+        print("Active Niyat Count for 6 month:", active_niyat_count_widget_for_6_month)
+        approval_pending_niyat_count_widget_for_6_month = int(next(line for line in self.page.inner_text(xpath_Approval_pending_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 6 month:", approval_pending_niyat_count_widget_for_6_month)
+        completed_niyat_count_widget_for_6_month = int(next(line for line in self.page.inner_text(xpath_Completed_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 6 month:", completed_niyat_count_widget_for_6_month)
+
+        # Execute SQL query to fetch data from the database
+        total_niyat_query_for_6_month= "6_month_niyat_tile_count_query"
+        headers, result = elaam_prod(total_niyat_query_for_6_month)
+        db_table_view= tabulate(result, headers=headers, tablefmt="grid")   # Display the database results in a tabular format
+        print("DB table view for 6 month: ", db_table_view)
+        # Process database results to store counts by status
+        total_niyat_count_db_for_6_month = result[0][headers.index('total_count')]
+        active_count_db = result[0][headers.index('active_count')]
+        completed_count_db = result[0][headers.index('completed_count')]
+        approval_pending_db = result[0][headers.index('approval_pending_count')]
+        deactivated_db = result[0][headers.index('deactivated_count')]
+        print("DB Total Niyat:", total_niyat_count_db_for_6_month)
+        print("DB Active:", active_count_db)
+        print("DB Completed:", completed_count_db)
+        print("DB Approval Pending:", approval_pending_db)
+        print("DB Deactivated:", deactivated_db)
+        total_niyat_count_add_all_status = active_count_db + completed_count_db + approval_pending_db + deactivated_db
+
+        assert total_niyat_count_widget_for_6_month == total_niyat_count_db_for_6_month, f"Expected  total niyats count  in db '{total_niyat_count_db_for_6_month}', but got on widget '{total_niyat_count_widget_for_6_month}'"
+        assert active_niyat_count_widget_for_6_month == active_count_db, f"Expected active niyat count in bd {active_count_db}, but got on widget {active_niyat_count_widget_for_6_month}"
+        assert approval_pending_niyat_count_widget_for_6_month == approval_pending_db, f"Expected approval pending niyat count in db {approval_pending_db}, but got on widget ' {approval_pending_niyat_count_widget_for_6_month}"
+        assert completed_niyat_count_widget_for_6_month == completed_count_db, f"Expected completed niyat count {completed_count_db}, but got on widget {completed_niyat_count_widget_for_6_month}"
+        assert total_niyat_count_add_all_status == total_niyat_count_db_for_6_month, f"Expected total niyat count in db {total_niyat_query_for_6_month}, but got total niyat count after adding all status{total_niyat_count_add_all_status}"
+        print("All assertions passed successfully for mumin dashboard tiles for 6 months filter")
+        self.page.wait_for_timeout(5000)
+
+    def click_duration_drop_down_and_select_1_year_filter(self):
+        self.page.locator(xpath_duration_dropdown).click()
+        self.page.locator("body").press("ControlOrMeta+Shift+I")
+        # self.page.get_by_role("option", name="Last 1 Month").click()
+        self.page.locator(xpath_1_year).click()
+
+    def verify_dashboard_tile_1_year_filter(self, elaam_prod):
+        self.page.wait_for_timeout(5000)
+        total_niyat_count_widget_for_1_year = int(next(line for line in self.page.inner_text(xpath_Total_Niyat_tile).splitlines() if line.strip()))
+        print("Total Niyat Count for 1 year:", total_niyat_count_widget_for_1_year)
+        active_niyat_count_widget_for_1_year  = int(next(line for line in self.page.inner_text(xpath_Active_Niyat_tile).splitlines() if line.strip()))
+        print("Active Niyat Count for 1 year:", active_niyat_count_widget_for_1_year)
+        approval_pending_niyat_count_widget_for_1_year = int(next(line for line in self.page.inner_text(xpath_Approval_pending_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 1 year:", approval_pending_niyat_count_widget_for_1_year)
+        completed_niyat_count_widget_for_1_year = int(next(line for line in self.page.inner_text(xpath_Completed_Niyat_tile).splitlines() if line.strip()))
+        print("Approval Pending Niyat Count for 1 year:", completed_niyat_count_widget_for_1_year)
+
+        # Execute SQL query to fetch data from the database
+        total_niyat_query_for_1_year= "1_year_niyat_tile_count_query"
+        headers, result = elaam_prod(total_niyat_query_for_1_year)
+        db_table_view= tabulate(result, headers=headers, tablefmt="grid")   # Display the database results in a tabular format
+        print("DB table view for 1 year: ", db_table_view)
+        # Process database results to store counts by status
+        total_niyat_count_db_for_1_year = result[0][headers.index('total_count')]
+        active_count_db = result[0][headers.index('active_count')]
+        completed_count_db = result[0][headers.index('completed_count')]
+        approval_pending_db = result[0][headers.index('approval_pending_count')]
+        deactivated_db = result[0][headers.index('deactivated_count')]
+        print("DB Total Niyat:", total_niyat_count_db_for_1_year)
+        print("DB Active:", active_count_db)
+        print("DB Completed:", completed_count_db)
+        print("DB Approval Pending:", approval_pending_db)
+        print("DB Deactivated:", deactivated_db)
+        total_niyat_count_add_all_status = active_count_db + completed_count_db + approval_pending_db + deactivated_db
+
+        assert total_niyat_count_widget_for_1_year == total_niyat_count_db_for_1_year, f"Expected  total niyats count  in db '{total_niyat_count_db_for_1_year}', but got on widget '{total_niyat_count_widget_for_1_year}'"
+        assert active_niyat_count_widget_for_1_year == active_count_db, f"Expected active niyat count in bd {active_count_db}, but got on widget {active_niyat_count_widget_for_1_year}"
+        assert approval_pending_niyat_count_widget_for_1_year == approval_pending_db, f"Expected approval pending niyat count in db {approval_pending_db}, but got on widget ' {approval_pending_niyat_count_widget_for_1_year}"
+        assert completed_niyat_count_widget_for_1_year == completed_count_db, f"Expected completed niyat count {completed_count_db}, but got on widget {completed_niyat_count_widget_for_1_year}"
+        assert total_niyat_count_add_all_status == total_niyat_count_db_for_1_year, f"Expected total niyat count in db {total_niyat_query_for_1_year}, but got total niyat count after adding all status{total_niyat_count_add_all_status}"
+        print("All assertions passed successfully for mumin dashboard tiles for 1 year filter")
+
+    def verify_total_trophy_count(self):
+        trophy_text = self.page.locator("//div[@class='mumnin_reward']").all_inner_texts()
+        print("Raw trophy text:", trophy_text)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
