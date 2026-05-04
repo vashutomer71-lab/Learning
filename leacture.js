05192c4296fee3010fce3c12fe9bd899e9b22f29
VashuTomer <vashutomer71@gmail.com>
2026-05-04 10:47:25 +0530

    remove all files from main

--- leacture.js
+++
@@ -1,7 +0,0 @@
-// Funcation and methods
-
-/* Functions--> A function is a block of code that performs a specific task. It is a reusable piece of code that can be called multiple times with different inputs. Functions help in breaking down a program into smaller, manageable parts, making it easier to read, maintain, and debug. They can take inputs (parameters) and return outputs (results). In JavaScript, functions can be defined using the function keyword or as arrow functions. They are fundamental building blocks in programming and are used to organize code and perform specific operations.*/
-
-/* Methods--> A method is a function that is associated with an object. It is a property of an object that contains a function definition. Methods are used to perform actions on objects or to manipulate the data within those objects. In JavaScript, methods are defined within objects and can be called using the dot notation. They allow us to interact with the properties of an object and perform specific tasks related to that object. For example, if we have an object representing a person, we can define a method called "greet" that returns a greeting message when called.*/   
-
-
